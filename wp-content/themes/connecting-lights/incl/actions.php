d094ebcab236e3f11ffcaf7cfb52b410f15192b3
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2012-08-29 12:58:39 -0400

    one more small change

--- wp-content/themes/connecting-lights/incl/actions.php
+++ wp-content/themes/connecting-lights/incl/actions.php
@@ -7,7 +7,7 @@
 			$e: ($("#send-message").detach().show()),
 			$trigger: $("#send-message-trigger"),
 			color_picker_src: "<?php bloginfo("template_url"); ?>/img/color-picker.png",
-			service_url: "/connecting-lights-backend/messages.json"
+			//service_url: "/connecting-lights-backend/messages.json"
 		});
 	});
 </script>
